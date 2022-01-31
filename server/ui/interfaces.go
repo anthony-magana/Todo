7be06147b124e72be3237ce119f1ea625bc81958
Anthony Magana <anthonymagana0705@gmail.com>
2022-01-30 19:15:40 -0800

    deletes tests

--- server/ui/interfaces.go
+++
@@ -1,7 +0,0 @@
-package ui
-
-import "github.com/anthony-magana/todo/server/entities"
-
-type Service interface {
-	GetAllTodos() ([]entities.Todo, error)
-}
--- server/usecases/interfaces.go
+++
@@ -1,7 +0,0 @@
-package usecases
-
-import "github.com/anthony-magana/todo/server/entities"
-
-type TodosRepository interface {
-	GetAllTodos() ([]entities.Todo, error)
-}
