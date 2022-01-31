2376fc56ab639159837f66242c4094d04f81c20f
Anthony Magana <anthonymagana0705@gmail.com>
2022-01-30 19:22:09 -0800

    Revert "deletes tests"
    This reverts commit 7be06147b124e72be3237ce119f1ea625bc81958.

---
+++ server/usecases/get_todos_test.go
@@ -0,0 +1,71 @@
+package usecases_test
+
+import (
+	"testing"
+	"fmt"
+	"github.com/anthony-magana/todo/server/entities"
+	"github.com/anthony-magana/todo/server/usecases"
+)
+
+var dummyTodos = []entities.Todo{
+	{
+		Title: "Todo 1",
+		Description: "Description 1",
+		Completed: true,
+	},
+	{
+		Title: "Todo 2",
+		Description: "Description 2",
+		Completed: true,
+	},
+	{
+		Title: "Todo 3",
+		Description: "Description 3",
+		Completed: true,
+	},
+}
+
+
+type badTodosRepo struct {}
+
+func (badTodosRepo) GetAllTodos() ([]entities.Todo, error) {
+	return nil, fmt.Errorf("Panic, something went wrong")
+}
+
+type MockTodosRepo struct {}
+
+func (MockTodosRepo) GetAllTodos() ([]entities.Todo, error) {
+	return dummyTodos, nil
+}
+
+func TestGetTodos(t *testing.T) {
+
+	// Test
+	t.Run("Should return ErrInternal when TodosRepository returns error", func(t *testing.T) {
+		repo := new(badTodosRepo)
+
+		todos, err := usecases.GetTodos(repo)
+
+		if err != usecases.ErrInternal {
+			t.Fatalf("Expected ErrInternal, got %v", err)
+		}
+		if todos != nil {
+			t.Fatalf("Expected todos to be nil, got %v", todos)
+		}
+	})
+	t.Run("Returns todos from TodosRepository", func(t *testing.T) {
+		repo := new(MockTodosRepo)
+
+		todos, err := usecases.GetTodos(repo)
+
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		if todos == nil {
+			t.Fatalf("Expected todos to be not nil, got %v", todos)
+		}
+		if len(todos) != len(dummyTodos) {
+			t.Fatalf("Expected todos to have %d elements, got %d", len(dummyTodos), len(todos))
+		}
+	})
+}
